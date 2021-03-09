c634ee46b953bdcbacdcfdf86a1fe1df98f0dc03
bigsaigon333 <likepeppermint@gmail.com>
2021-03-09 23:10:38 +0900

    feat: dummy search data를 반환하는 endpoint를 구현한다

---
+++ dummy/fetchData.js
@@ -0,0 +1,7 @@
+const dummySearchData = require("./searchData.js");
+
+module.exports = () => {
+  const randomIndex = Math.floor(Math.random() * dummySearchData.length);
+
+  return Promise.resolve(dummySearchData[randomIndex]);
+};
---
+++ functions/dummy.js
@@ -0,0 +1,29 @@
+const fetchDummySearchData = require("../dummyfetchData.js");
+
+exports.handler = async (event) => {
+  const headers = {
+    "Access-Control-Allow-Origin": process.env.HOST,
+    Vary: "Origin",
+  };
+
+  try {
+    const response = await fetchDummySearchData();
+    const body = await response.json();
+
+    console.log(body);
+
+    return {
+      statusCode: 200,
+      ok: true,
+      headers,
+      body: JSON.stringify(body, null, " "),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      ok: false,
+      headers,
+      body: JSON.stringify(error, null, " "),
+    };
+  }
+};
