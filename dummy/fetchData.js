d0bbf7ea6218b1a98ec529afc1e59636cfc01c40
bigsaigon333 <likepeppermint@gmail.com>
2021-03-09 23:21:10 +0900

    fix: node module import를 수정한다

--- dummy/fetchData.js
+++ dummy/fetchData.js
@@ -1,7 +1,9 @@
-const dummySearchData = require("./searchData.js");
+const { data: dummySearchData } = require("./searchData.js");
 
 module.exports = () => {
   const randomIndex = Math.floor(Math.random() * dummySearchData.length);
+
+  console.log(dummySearchData[randomIndex]);
 
   return Promise.resolve(dummySearchData[randomIndex]);
 };
