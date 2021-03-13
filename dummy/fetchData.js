e6910e399705368219121e30e6567d152c80cbd0
bigsaigon333 <likepeppermint@gmail.com>
2021-03-13 23:28:16 +0900

    fix: 불필요한 console.log를 삭제한다

--- dummy/fetchData.js
+++ dummy/fetchData.js
@@ -3,7 +3,5 @@ const { data: dummySearchData } = require("./searchData.js");
 module.exports = () => {
   const randomIndex = Math.floor(Math.random() * dummySearchData.length);
 
-  console.log(dummySearchData[randomIndex]);
-
   return Promise.resolve(dummySearchData[randomIndex]);
 };
